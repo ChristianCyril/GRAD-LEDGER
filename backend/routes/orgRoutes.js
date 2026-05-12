41edf906a0a51b8c8813e2837b0a8caca3ac5ed3
ChristianCyril <christiancyril78@gmail.com>
2026-05-12 06:12:09 +0100

    feat: update .gitignore and add libphonenumber-js dependency. Also included the upload middleware on my organisation registration route.

--- backend/routes/orgRoutes.js
+++ backend/routes/orgRoutes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/authenticate.js";
 import { authorise } from "../middleware/authorise.js";
+import { upload } from "../middleware/uploadMiddleware.js";
 import {registerOrganisation,updateOrgProfile} from '../controller/orgController.js'
 
 const router = Router()
 
-router.post('/register',registerOrganisation)
+router.post('/register', upload.fields([
+  { name: 'doc_incorporation', maxCount: 1 },
+  { name: 'doc_letter_of_intent', maxCount: 1 },
+  { name: 'doc_accreditation', maxCount: 1 }
+]), registerOrganisation)
 router.patch('/profile',authenticate,authorise('ORG_SUPER_ADMIN'),updateOrgProfile)
 
 export default router
